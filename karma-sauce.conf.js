13922e6b08fc3931ff712b9e69f18bd281fbb955
Marco Solazzi <marco.solazzi@gmail.com>
2016-10-29 15:30:26 +0200

    saucelab testing

--- karma-sauce.conf.js
+++ karma-sauce.conf.js
@@ -2,8 +2,62 @@
 // Generated on Wed Oct 26 2016 17:54:27 GMT+0200 (CEST)
 
 const path = require('path');
+const version = require('./package.json').version;
 
 module.exports = function(config) {
+
+
+  const customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 10',
+      version: 'latest'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'Windows 10',
+      version: 'latest'
+    },
+
+    sl_mac_safari: {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.10'
+    },
+
+    sl_ie_9: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '9'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    },
+    sl_edge: {
+      base: 'SauceLabs',
+      browserName: 'MicrosoftEdge',
+      platform: 'Windows 10'
+    },
+
+    sl_android_5_1: {
+      base: 'SauceLabs',
+      browserName: 'android',
+      version: '5.1'
+    },
+    sl_ios_safari_9: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      version: '9.3'
+    },
+  }
+
+
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -53,11 +107,6 @@ module.exports = function(config) {
       stats: 'errors-only'
     },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
-
     // web server port
     port: 9876,
 
@@ -71,16 +120,27 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     //autoWatch: true,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //browsers: ['PhantomJS', 'Chrome', 'Firefox', 'IE', 'Safari', 'Opera'],
+    sauceLabs: {
+        testName: 'vue-types',
+        recordScreenshots: false,
+        build: version + '-' + Date.now()
+    },
+
+    customLaunchers: customLaunchers,
+
+    browsers: Object.keys(customLaunchers),
+
+    reporters: ['mocha', 'saucelabs'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    //singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    captureTimeout: 300000,
+    browserNoActivityTimeout: 300000
   })
 }
