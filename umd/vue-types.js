7fa3bd30fd5e8dbcea522bd6c34ee95d2f596e3b
Marco Solazzi <marco.solazzi+git@aquest.it>
2016-11-14 10:24:33 +0100

    new build

--- umd/vue-types.js
+++ umd/vue-types.js
@@ -10,9 +10,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["VuePropTypes"] = factory();
+		exports["VueTypes"] = factory();
 	else
-		root["VuePropTypes"] = factory();
+		root["VueTypes"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
