3704ce9cda057b92799bd9f6509e7f52a9c06dea
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-04-04 11:34:46 +0200

    enableing tests for valodator function

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -3,10 +3,18 @@ import * as utils from '../src/utils'
 
 describe('`toType()`', () => {
 
+  it('should enhance the passed-in object without cloning', () => {
+    const obj = {}
+
+    const type = utils.toType('testType', obj)
+    expect(type).toBe(obj)
+
+  })
+
   it('should call `withRequired` on passed in object', () => {
     const obj = {}
 
-    utils.toType(obj)
+    utils.toType('testType', obj)
     expect(obj.hasOwnProperty('isRequired')).toBe(true)
 
   })
@@ -14,22 +22,22 @@ describe('`toType()`', () => {
   it('should call `withDefault` on passed in object', () => {
     const obj = {}
 
-    utils.toType(obj)
+    utils.toType('testType', obj)
     expect(obj.def).toBeA(Function)
 
   })
 
-  // it('should bind provided `validator function to the passed in object`', () => {
-  //   const obj = {
-  //     validator() {
-  //       return this
-  //     }
-  //   }
-  //
-  //   const type = utils.toType(obj)
-  //   const validator = type.validator
-  //
-  //   expect(validator()).toBe(obj)
-  // })
+  it('should bind provided `validator function to the passed in object`', () => {
+    const obj = {
+      validator() {
+        return this
+      }
+    }
+
+    const type = utils.toType('testType', obj)
+    const validator = type.validator
+
+    expect(validator()).toBe(obj)
+  })
 
 })
