4b61b87601b6fa90f9ec3d46dca66ca58dcb0e48
Marco Solazzi <marco.solazzi@gmail.com>
2018-05-16 19:08:26 +0900

    type linting

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,47 +1,42 @@
-import { Prop, PropOptions } from 'vue/types/options'
+// TypeScript Version: 2.3
+import { Prop, PropOptions } from 'vue/types/options';
 
-export as namespace VueTypes;
-
-export interface IVueTypeDef extends PropOptions {
-  readonly _vueTypes_name: string
-  readonly def: <T>(def: T) => this & { default: T },
-  readonly isRequired: this & { required: boolean },
-  _vueTypes_isLoose?: boolean
-  readonly loose?: this & { _vueTypes_isLoose: boolean }
+export interface VueTypeDef extends PropOptions {
+  readonly _vueTypes_name: string;
+  readonly def: <T>(def: T) => this & { default: T };
+  readonly isRequired: this & { required: boolean };
+  _vueTypes_isLoose?: boolean;
+  readonly loose?: this & { _vueTypes_isLoose: boolean };
 }
 
-export interface IConstructor {
-  new (...args: any[]): any
+export interface Constructor {
+  new (...args: any[]): any;
 }
 
-export type VueProp = IVueTypeDef | PropOptions<any>
+export type VueProp = VueTypeDef | PropOptions;
 
-export interface IUtils {
-  validate(value: any, type: VueProp | Prop<any> | Array<Prop<any>>): boolean
-  toType(name: string, obj: PropOptions): IVueTypeDef
+export interface Utils {
+  validate(value: any, type: VueProp | Prop<any> | Array<Prop<any>>): boolean;
+  toType(name: string, obj: PropOptions): VueTypeDef;
 }
 
-export interface IVueTypes {
-  sensibleDefaults?: {},
-  utils: IUtils
-  readonly any: IVueTypeDef
-  readonly bool: IVueTypeDef
-  readonly func: IVueTypeDef
-  readonly array: IVueTypeDef
-  readonly string: IVueTypeDef
-  readonly number: IVueTypeDef
-  readonly object: IVueTypeDef
-  readonly integer: IVueTypeDef
-  readonly symbol: IVueTypeDef
-  custom(fn: (...value: any[]) => boolean, warnMsg?: string): IVueTypeDef
-  oneOf<T>(arr: T[]): IVueTypeDef,
-  instanceOf(instanceConstructor: IConstructor): IVueTypeDef
-  oneOfType(arr: Array<Prop<any> | VueProp>): IVueTypeDef
-  arrayOf(type: IVueTypeDef | Prop<any>): IVueTypeDef
-  objectOf(type: IVueTypeDef | Prop<any>): IVueTypeDef
-  shape(obj: { [key: string]: VueProp|Prop<any> }): IVueTypeDef
+export interface VueTypes {
+  sensibleDefaults?: {};
+  utils: Utils;
+  readonly any: VueTypeDef;
+  readonly bool: VueTypeDef;
+  readonly func: VueTypeDef;
+  readonly array: VueTypeDef;
+  readonly string: VueTypeDef;
+  readonly number: VueTypeDef;
+  readonly object: VueTypeDef;
+  readonly integer: VueTypeDef;
+  readonly symbol: VueTypeDef;
+  custom(fn: (...value: any[]) => boolean, warnMsg?: string): VueTypeDef;
+  oneOf(arr: any[]): VueTypeDef;
+  instanceOf(instanceConstructor: Constructor): VueTypeDef;
+  oneOfType(arr: Array<Prop<any> | VueProp>): VueTypeDef;
+  arrayOf(type: VueTypeDef | Prop<any>): VueTypeDef;
+  objectOf(type: VueTypeDef | Prop<any>): VueTypeDef;
+  shape(obj: { [key: string]: VueProp|Prop<any> }): VueTypeDef;
 }
-
-declare const VueTypes: IVueTypes;
-
-export default VueTypes;
