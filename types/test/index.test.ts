d9ca131ca931ae6ec555d1f1b6c6e34ee6c9cb7f
Marco Solazzi <marco.solazzi@gmail.com>
2019-02-13 16:44:41 +0900

    typings and demo

--- types/test/index.test.ts
+++ types/test/index.test.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import VueTypes from '../index';
+import { VueTypesInterface, VueTypeDef, VueTypeValidableDef } from '../vue-types';
 
 const noop = () => { };
 
@@ -75,6 +76,20 @@ shapeType.type = Object;
 VueTypes.sensibleDefaults = {};
 VueTypes.sensibleDefaults = false;
 VueTypes.sensibleDefaults = true;
+
+interface CustomVueTypes extends VueTypesInterface {
+  readonly test: VueTypeValidableDef<any>;
+}
+// extending
+const myTypes = VueTypes.extend<CustomVueTypes>({
+  name: 'test',
+  validate: true,
+  getter: true
+});
+
+(VueTypes as CustomVueTypes).test.isRequired;
+myTypes.test.isRequired;
+myTypes.test.isRequired;
 
 const BaseComponent = Vue.extend({
   props: {
