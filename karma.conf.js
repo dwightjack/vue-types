5d6782c1b7c52b7d84a1fc048657068e35003f92
dwightjack <marco.solazzi@gmail.com>
2020-07-15 17:40:52 +0900

    exclude build files from test preprocess

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,7 @@ module.exports = (config) => {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.ts': 'karma-typescript',
+      'src/**/*.ts': 'karma-typescript',
       'test/**/*.ts': 'karma-typescript',
     },
 
