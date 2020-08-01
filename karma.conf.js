14a4edea755b6e733e08d6f2180862d836b0dd9f
Marco Solazzi <marco.solazzi@gmail.com>
2020-08-01 13:36:50 +0900

    test: run tests in order

--- karma.conf.js
+++ karma.conf.js
@@ -80,5 +80,11 @@ module.exports = (config) => {
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
+    client: {
+      jasmine: {
+        random: false,
+      },
+    },
   })
 }
