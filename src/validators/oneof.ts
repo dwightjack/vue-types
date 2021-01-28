353df5a534d839375735424435d2374b2411e685
FAKER-A <1037881273@qq.com>
2021-01-28 18:20:10 +0800

    fix: oneof warning message

--- src/validators/oneof.ts
+++ src/validators/oneof.ts
@@ -7,7 +7,7 @@ export default function oneOf<T extends readonly any[]>(arr: T) {
       '[VueTypes error]: You must provide an array as argument.',
     )
   }
-  const msg = `oneOf - value should be one of "${arr.join('", "')}."`
+  const msg = `oneOf - value should be one of "${arr.join('", "')}".`
   const allowedTypes = arr.reduce((ret, v) => {
     if (v !== null && v !== undefined) {
       const constr = (v as any).constructor
