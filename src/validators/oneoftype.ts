797762980a0c8e8785f9447f4ad18e150c8f3541
Victor Garcia <victorgarciaparis13@gmail.com>
2021-07-13 09:57:13 +0200

    Removed tests

--- src/validators/oneoftype.ts
+++ src/validators/oneoftype.ts
@@ -10,10 +10,6 @@ import {
   indent,
 } from '../utils'
 
-type RawLocation = string | { name: string }
-const ctor = oneOfType([String, Object]).def('hello') // Valid
-const route = oneOfType<RawLocation>([String, Object]).def('hello') // Valid
-
 export default function oneOfType<
   D extends V,
   U extends VueProp<any> | Prop<any> = any,
