b007a79779349f01643a0fac6849fa45b896ca2f
dwightjack <marco.solazzi@gmail.com>
2025-08-18 16:33:52 +0900

    update deps and add playground for nuxt 4

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
-import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createRequire } from 'node:module'
 
-const resolve = (name: string) => fileURLToPath(import.meta.resolve(name))
+const require = createRequire(import.meta.url)
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   resolve: {
     alias: {
-      vue: resolve('vue/dist/vue.esm-browser.js'),
-      'vue-types': resolve('../core/src/index.ts'),
+      vue: require.resolve('vue/dist/vue.esm-browser.js'),
+      'vue-types': resolve(__dirname, '../core/src/index.ts'),
     },
   },
 })
